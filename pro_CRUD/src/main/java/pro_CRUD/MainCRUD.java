79aa62cce041cfeeb9ad85b5702b6d674d24c28e
김민정 <mjkim8038@raoncorp.com>
2021-05-24 09:08:46 +0900

    자바 컴파일 버전 변경

--- pro_CRUD/src/main/java/pro_CRUD/MainCRUD.java
+++ pro_CRUD/src/main/java/pro_CRUD/MainCRUD.java
@@ -5,6 +5,7 @@ public class MainCRUD {
 	public static void main(String[] args) {
 		System.out.println("second project");
 		int i = 100;
+		
 		System.out.println("스태시 한글깨짐1 " + i);
 		System.out.println("스태시 한글깨짐2 " + i);
 		
