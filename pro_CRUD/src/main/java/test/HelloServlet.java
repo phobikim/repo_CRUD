067ae11e51f161b2f197a79d9e782efb5f2225a3
김민정 <mjkim8038@raoncorp.com>
2021-05-24 15:23:37 +0900

    #패키지 구조 변경

--- pro_CRUD/src/main/java/test/HelloServlet.java
+++ pro_CRUD/src/main/java/test/HelloServlet.java
@@ -1,8 +1,7 @@
+package test;
 
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,16 +9,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class LoginServlet
+ * Servlet implementation class HelloServlet
  */
-@WebServlet("/login")
-public class LoginServlet extends HttpServlet {
+//@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LoginServlet() {
+    public HelloServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -29,12 +28,7 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.setContentType("text/html;charset=UTF-8");
-		String id = request.getParameter("id");
-		String passwd = request.getParameter("passwd");
-		PrintWriter out = response.getWriter();
-		out.print("아이디: " + id + "<br>");
-		out.print("패스워드: " + passwd + "<br>");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
--- pro_CRUD/src/main/java/test/LoginServlet.java
+++ pro_CRUD/src/main/java/test/LoginServlet.java
@@ -1,6 +1,9 @@
+package test;
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,16 +11,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloServlet
+ * Servlet implementation class LoginServlet
  */
-//@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloServlet() {
+    public LoginServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,7 +30,14 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		//getParameter("") <-- name="" 과 연결
+		String id = request.getParameter("id");
+		String passwd = request.getParameter("passwd");
+		PrintWriter out = response.getWriter();
+		out.print("아이디: " + id + "<br>");
+		out.print("패스워드: " + passwd + "<br>");
+		out.close();
 	}
 
 	/**
