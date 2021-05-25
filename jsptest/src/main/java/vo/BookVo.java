a1138e03b6f178a96a8d004e0591e479f2bac1b8
김민정 <mjkim8038@raoncorp.com>
2021-05-25 16:54:11 +0900

    #lombok 설정 후 BookVo 생성

--- jsptest/src/main/java/vo/BookVo.java
+++ jsptest/src/main/java/vo/BookVo.java
@@ -1,5 +1,24 @@
 package vo;
 
-public class BookVo {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class BookVo {
+	private int no;
+	private String name;
+	private String publisher;
+	private String writer;
+	private int price;
+	
+
+	
 }
+
+
